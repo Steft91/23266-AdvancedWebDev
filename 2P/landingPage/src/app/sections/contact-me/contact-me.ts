3af00ae3a07ce0de3f382025bbbca2e5e366f58b
Steft91 <stefany152005@gmail.com>
2025-07-03 19:57:23 -0500

    arreglos finales contact me

--- 2P/landingPage/src/app/sections/contact-me/contact-me.ts
+++ 2P/landingPage/src/app/sections/contact-me/contact-me.ts
@@ -8,17 +8,67 @@ import { Component, signal } from '@angular/core';
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.css'
 })
-export class ContactMe {
-
-  // signal 
+export class ContactMe { 
+  // Toasts
   showToast = signal(false);
+  toastMensaje = signal('');
+
+  nombre = signal('');
+  apellido = signal('');
+  correo = signal('');
+  telefono = signal('');
+  mensaje = signal('');
+
+  actualizarNombre(event: Event) {
+    this.nombre.set((event.target as HTMLInputElement).value);
+  }
+
+  actualizarApellido(event: Event) {
+    this.apellido.set((event.target as HTMLInputElement).value);
+  }
+
+  actualizarCorreo(event: Event) {
+    this.correo.set((event.target as HTMLInputElement).value);
+  }
+
+  actualizarTelefono(event: Event) {
+    this.telefono.set((event.target as HTMLInputElement).value);
+  }
+
+  actualizarMensaje(event: Event) {
+    this.mensaje.set((event.target as HTMLTextAreaElement).value);
+  }
+
+  
 
   onSubmit(event: Event) {
     event.preventDefault();
+
+    if (
+      this.nombre().trim() === '' ||
+      this.apellido().trim() === '' ||
+      this.correo().trim() === '' ||
+      this.telefono().trim() === '' ||
+      this.mensaje().trim() === ''
+    ) {
+      this.toastMensaje.set('<i class="fa-solid fa-circle-exclamation"></i> Recuerde llenar todos los campos');
+      this.showToast.set(true);
+      setTimeout(() => this.showToast.set(false), 3000);
+      return;
+    }
+
+    // Envío exitoso
+    this.toastMensaje.set('<i class="fa-solid fa-circle-check"></i> Formulario enviado exitosamente');
     this.showToast.set(true);
-    // ocultar toast después de 3 segundos
-    setTimeout(() => {
-      this.showToast.set(false);
-    }, 3000);
+    setTimeout(() => this.showToast.set(false), 3000);
+
+    this.nombre.set('');
+    this.apellido.set('');
+    this.correo.set('');
+    this.telefono.set('');
+    this.mensaje.set('');
   }
+
+  
 }
+
